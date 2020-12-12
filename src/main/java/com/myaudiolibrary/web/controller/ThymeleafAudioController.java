54e7e943e2eb6495d1a37b85b1c33d8dd4225186
Dylan ANTONIOTTI <dylan.antoniotti@gmail.com>
2020-12-12 18:15:25 +0100

    Eval thymeleaf

--- src/main/java/com/myaudiolibrary/web/controller/ThymeleafAudioController.java
+++ src/main/java/com/myaudiolibrary/web/controller/ThymeleafAudioController.java
@@ -1,6 +1,8 @@
 package com.myaudiolibrary.web.controller;
 
+import com.myaudiolibrary.web.model.Album;
 import com.myaudiolibrary.web.model.Artist;
+import com.myaudiolibrary.web.repository.AlbumRepository;
 import com.myaudiolibrary.web.repository.ArtistRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.view.RedirectView;
 
 @Controller
 @RequestMapping(value = "/thymeleaf")
@@ -19,6 +22,9 @@ public class ThymeleafAudioController {
 
     @Autowired
     private ArtistRepository artistRepository;
+
+    @Autowired
+    private AlbumRepository albumRepository;
 
     @RequestMapping(method = RequestMethod.GET)
     public String index(final ModelMap model) {
@@ -29,24 +35,31 @@ public class ThymeleafAudioController {
     public String getArtistById(final ModelMap model, @PathVariable Long artistId) {
         Artist artist = artistRepository.findById(artistId).orElse(null);
         if(artist != null) {
-            model.put("nameArtist", artist.getName());
-            model.put("albums", artist.getAlbums());
+            model.put("artist", artist);
         }
         return "detailArtist";
     }
 
     @RequestMapping(method = RequestMethod.GET, value = "/artists", params = "name")
     public String getArtistByName(final ModelMap model, @RequestParam String name, @RequestParam(defaultValue = "0") Integer page, @RequestParam(defaultValue = "10") Integer size, @RequestParam(defaultValue = "name") String sortProperty, @RequestParam(defaultValue = "ASC") String sortDirection) {
-        Page<Artist> artists = artistRepository.findByName(name, PageRequest.of(page, size, Sort.Direction.fromString(sortDirection), sortProperty));
+        Page<Artist> artists = artistRepository.findByNameContains(name, PageRequest.of(page, size, Sort.Direction.fromString(sortDirection), sortProperty));
         if(!artists.isEmpty()) {
             model.put("artists", artists);
             model.put("start", (page) * size + 1);
             model.put("end", (page) * size + artists.getNumberOfElements());
             model.put("totalArtist", artists.getTotalElements());
+            model.put("previousPage", page - 1);
+            model.put("currentPage", page);
+            model.put("nextPage", page + 1);
+            model.put("isLastPage", artists.isLast());
         } else {
             model.put("start", 0);
             model.put("end", 0);
             model.put("totalArtist", 0);
+            model.put("previousPage", 0);
+            model.put("currentPage", 0);
+            model.put("nextPage", 0);
+            model.put("isLastPage", true);
         }
         return "listeArtists";
     }
@@ -59,12 +72,59 @@ public class ThymeleafAudioController {
             model.put("start", (page) * size + 1);
             model.put("end", (page) * size + artists.getNumberOfElements());
             model.put("totalArtist", artists.getTotalElements());
+            model.put("previousPage", page - 1);
+            model.put("currentPage", page);
+            model.put("nextPage", page + 1);
+            model.put("isLastPage", artists.isLast());
         } else {
             model.put("start", 0);
             model.put("end", 0);
             model.put("totalArtist", 0);
+            model.put("previousPage", 0);
+            model.put("currentPage", 0);
+            model.put("nextPage", 0);
+            model.put("isLastPage", true);
         }
 
         return "listeArtists";
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value = "/artists/new")
+    public String getPageCreationArtist(final ModelMap model) {
+        model.put("artist", new Artist());
+
+        return "detailArtist";
+    }
+
+    @RequestMapping(method = RequestMethod.POST, value = "/artists")
+    public String saveArtist(final ModelMap model, Artist artistCreated) {
+        //Check if exist
+        artistRepository.save(artistCreated);
+        model.put("artist", artistCreated);
+        return "detailArtist";
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value = "/artists/{idArtist}/delete")
+    public RedirectView deleteArtist(@PathVariable Long idArtist) {
+        Artist artist = artistRepository.findById(idArtist).orElse(null);
+        if(artist != null) {
+            artistRepository.delete(artist);
+            return new RedirectView("/thymeleaf/artists?page=0&size=10&sortProperty=name&sortDirection=ASC");
+        } else {
+            //404
+            return new RedirectView("/thymeleaf");
+        }
+    }
+
+    @RequestMapping(method = RequestMethod.POST, value = "/artists/{idArtist}/album")
+    public String createAlbum(final ModelMap model, @PathVariable Long idArtist, Album album) {
+        Artist artist = artistRepository.findById(idArtist).orElse(null);
+        if(artist != null) {
+            album.setArtist(artist);
+            albumRepository.save(album);
+            model.put("artist", artist);
+        }
+
+        return "detailArtist";
     }
 }
